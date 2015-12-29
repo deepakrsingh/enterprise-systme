466315343132de64b4bbd73d23f52a3f553f5c49
Deepak Singh <deepak.neosoft@gmail.com>
2015-12-29 20:01:00 +0530

    Master-Copy

---
+++ Enterprise-System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Enterprise_System.Startup))]
+namespace Enterprise_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
